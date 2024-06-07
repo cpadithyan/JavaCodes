225f59deda7a9c4eb796c72513484836537f91e1
Adithyan cp <90973311+cpadithyan@users.noreply.github.com>
2024-06-07 22:33:41 +0530

    1.2

---
+++ com/adithyan/PrimeCheck.java
@@ -0,0 +1,24 @@
+package com.adithyan;
+
+import java.util.Scanner;
+
+public class PrimeCheck{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = scanner.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println(num + "is prime");
+        }else{
+            System.out.println(num+ "is not prime");
+        }
+    }
+    public static boolean isPrime(int num){
+        if (num<=1) return false;
+        for (int i=2; i<=Math.sqrt(num); i++){
+            if (num%i==0) return false;
+        }
+        return true;
+    }
+}
