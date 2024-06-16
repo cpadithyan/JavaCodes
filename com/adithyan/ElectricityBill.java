341015f9f28feaaf3eeab11097eae42959d2b571
Adithyan cp <90973311+cpadithyan@users.noreply.github.com>
2024-06-16 23:07:20 +0530

    1.11

---
+++ com/adithyan/ElectricityBill.java
@@ -0,0 +1,21 @@
+package com.adithyan;
+
+import java.util.Scanner;
+
+public class ElectricityBill {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter units consumed : ");
+        int units = scanner.nextInt();;
+        double bill = 0;
+
+        if (units<=100){
+            bill = units*1.20;
+        }else if (units<=200) {
+            bill = 100*1.20 + (units-100)*2;
+        } else if (units<=300) {
+            bill = 100*1.20 + 200*2 + (units - 300)*3;
+        }
+        System.out.println("The bill is : "+bill);
+    }
+}
